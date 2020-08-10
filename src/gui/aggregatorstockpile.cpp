133b925c44cc8a4cb279f14264d8bd0f6f63733e
otreblan <otreblain@gmail.com>
2020-08-10 14:23:28 -0500

    Case again

--- src/gui/aggregatorstockpile.cpp
+++ src/gui/aggregatorstockpile.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include "AggregatorStockpile.h"
+#include "aggregatorstockpile.h"
 
 #include "../base/counter.h"
 #include "../base/db.h"
