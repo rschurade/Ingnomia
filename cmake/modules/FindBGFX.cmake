b129b87f87675c5ecbf04f0c855e7bb171c15e76
Edu Garcia <28616+Arcnor@users.noreply.github.com>
2022-07-19 16:24:50 +0200

    chore: missing FindBGFX CMake code

---
+++ cmake/modules/FindBGFX.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+
+set(BGFX_OPENGL_VERSION 41 CACHE STRING "Set BGFX OpenGL version to 4.1 (max version on macOS)" FORCE)
+FetchContent_Declare(
+        BGFX
+        GIT_REPOSITORY "https://github.com/bkaradzic/bgfx.cmake.git"
+        GIT_TAG "896ab5478cd943c51fe14ff8e4a1abf7b4102bca")
+FetchContent_MakeAvailable(BGFX)
